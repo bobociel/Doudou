ce52281108c58970ededdaff91ae974e05a45c1b
wangxiaobo <wangxiaobo@lovewith.me>
2016-08-12 16:30:40 +0800

    SwiftPerfectDemo

---
+++ Swift/PerfectDemo2/PerfectDemo2/PerfectDemo2.h
@@ -0,0 +1,19 @@
+//
+//  PerfectDemo2.h
+//  PerfectDemo2
+//
+//  Created by wangxiaobo on 16/8/12.
+//  Copyright © 2016年 com.lovewith.lovewith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PerfectDemo2.
+FOUNDATION_EXPORT double PerfectDemo2VersionNumber;
+
+//! Project version string for PerfectDemo2.
+FOUNDATION_EXPORT const unsigned char PerfectDemo2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerfectDemo2/PublicHeader.h>
+
+
