983792156c31de01f4be2e8fd8147cd9ce012f59
wangxiaobo <wangxiaobo@lovewith.me>
2016-08-13 14:38:48 +0800

    同上

---
+++ Swift/SwiftPerfect/PerfectDemo/PerfectDemo/PerfectModule.swift
@@ -0,0 +1,23 @@
+//
+//  PerfectModule.swift
+//  PerfectDemo
+//
+//  Created by wangxiaobo on 16/8/13.
+//  Copyright © 2016年 com.lovewith.lovewith. All rights reserved.
+//
+
+import Foundation
+import PerfectLib
+import MySQL
+
+class PerfectServerModuleInit {
+    
+}
+
+class Handle: RequestHandler {
+
+    //handle request
+    func handleRequest(request: WebRequest, response: WebResponse) {
+
+    }
+}
